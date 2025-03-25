c379959661e17a5a23c7294bf850742cd12bc321
Gabor Szabo <gabor@szabgab.com>
2025-03-25 15:39:18 +0200

    convert more functions to methods

--- src/lib.rs
+++ src/lib.rs
@@ -61,85 +61,91 @@ impl Repository {
     }
 
     pub fn path(&self, root: &Path) -> PathBuf {
-        root.join(&self.host).join(&self.owner).join(&self.repo)
+        self.owner_path(root).join(&self.repo)
     }
-}
 
-/// Run `git clone` or `git pull` to update a single repository
-pub fn update_single_repository(
-    repos_folder: &Path,
-    host: &str,
-    owner: &str,
-    repo: &str,
-    repository_url: &str,
-    clone: bool,
-) -> Result<(), Box<dyn Error>> {
-    let owner_path = repos_folder.join(host).join(owner);
-    let current_dir = env::current_dir()?;
-    log::info!(
-        "Creating owner_path {:?} while current_dir is {:?}",
-        &owner_path,
-        &current_dir
-    );
-    fs::create_dir_all(&owner_path)?;
-    let repo_path = owner_path.join(repo);
-    if Path::new(&repo_path).exists() {
-        if clone {
-            log::info!("repo exist but we only clone now.  Skipping.");
+    pub fn owner_path(&self, root: &Path) -> PathBuf {
+        root.join(&self.host).join(&self.owner)
+    }
+
+    /// Run `git clone` or `git pull` to update a single repository
+    pub fn update_repository(&self, root: &Path, clone: bool) -> Result<(), Box<dyn Error>> {
+        let owner_path = self.owner_path(root);
+        let current_dir = env::current_dir()?;
+        log::info!(
+            "Creating owner_path {:?} while current_dir is {:?}",
+            &owner_path,
+            &current_dir
+        );
+        fs::create_dir_all(&owner_path)?;
+        let repo_path = self.path(root);
+        if Path::new(&repo_path).exists() {
+            if clone {
+                log::info!("repo exist but we only clone now.  Skipping.");
+            } else {
+                log::info!("repo exist; cd to {:?}", &repo_path);
+                env::set_current_dir(&repo_path)?;
+                self.git_pull();
+            }
         } else {
-            log::info!("repo exist; cd to {:?}", &repo_path);
-            env::set_current_dir(&repo_path)?;
-            git_pull();
+            log::info!("new repo; cd to {:?}", &owner_path);
+            env::set_current_dir(owner_path)?;
+            self.git_clone(root);
         }
-    } else {
-        log::info!("new repo; cd to {:?}", &owner_path);
-        env::set_current_dir(owner_path)?;
-        git_clone(repository_url, repo);
+        env::set_current_dir(current_dir)?;
+        Ok(())
     }
-    env::set_current_dir(current_dir)?;
-    Ok(())
-}
 
-fn git_pull() {
-    log::info!("git pull");
-    let current_dir = env::current_dir().unwrap();
+    fn git_pull(&self) {
+        log::info!("git pull");
+        let current_dir = env::current_dir().unwrap();
 
-    match Command::new("git").arg("pull").output() {
-        Ok(result) => {
-            if result.status.success() {
-                log::info!(
-                    "git_pull exit code: '{}' in folder {:?}",
-                    result.status,
-                    current_dir
-                );
-            } else {
-                log::warn!(
-                    "git_pull exit code: '{}' in folder {:?}",
-                    result.status,
-                    current_dir
-                );
+        match Command::new("git").arg("pull").output() {
+            Ok(result) => {
+                if result.status.success() {
+                    log::info!(
+                        "git_pull exit code: '{}' in folder {:?}",
+                        result.status,
+                        current_dir
+                    );
+                } else {
+                    log::warn!(
+                        "git_pull exit code: '{}' in folder {:?}",
+                        result.status,
+                        current_dir
+                    );
+                }
+            }
+            Err(err) => {
+                log::error!("Could not run git_pull in folder {current_dir:?} error: {err}")
             }
         }
-        Err(err) => log::error!("Could not run git_pull in folder {current_dir:?} error: {err}"),
     }
-}
 
-fn git_clone(url: &str, path: &str) {
-    log::info!("git clone {} {}", url, path);
-    match Command::new("git").arg("clone").arg(url).arg(path).output() {
-        Ok(result) => {
-            if result.status.success() {
-                log::info!("git_clone exit code: '{}'", result.status);
-            } else {
-                log::warn!(
-                    "git_clone exit code: '{}' for url '{}' cloning to '{}'",
-                    result.status,
-                    url,
-                    path
-                );
+    fn git_clone(&self, root: &Path) {
+        let url = self.url();
+        let path = self.path(root);
+        log::info!("git clone {} {:?}", url, path);
+        match Command::new("git")
+            .arg("clone")
+            .arg(self.url())
+            .arg(&path)
+            .output()
+        {
+            Ok(result) => {
+                if result.status.success() {
+                    log::info!("git_clone exit code: '{}'", result.status);
+                } else {
+                    log::warn!(
+                        "git_clone exit code: '{}' for url '{}' cloning to '{:?}'",
+                        result.status,
+                        url,
+                        path
+                    );
+                }
             }
+            Err(err) => log::error!("Could not run git_clone {url} {path:?} error: {err}"),
         }
-        Err(err) => log::error!("Could not run git_clone {url} {path} error: {err}"),
     }
 }
 
